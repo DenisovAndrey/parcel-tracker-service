a260b98629fb89baf9eea68aa6113c0c2936da19
Andrey Denisov <andrey.denisov@westwing.de>
2023-09-06 14:09:12 +0200

    chore: add DHLOrderRawDataResponse

---
+++ src/types/suppliers/DHLInractions.ts
@@ -0,0 +1,14 @@
+export interface DHLOrderRawDataResponse {
+  orderNo: string;
+  tracking_number: string;
+  courier: string;
+  street: string;
+  zip_code: string;
+  city: string;
+  destination_country_iso3: string;
+  email: string;
+  articleNo: string;
+  articleImageUrl: string;
+  quantity: string;
+  product_name: string;
+}
